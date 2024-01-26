fcedb4edd0d8dfbaac018d49fb673e5a2d9480cb
salaudeen <109912988+salaudeen22@users.noreply.github.com>
2024-01-26 23:47:19 +0530

    Update index2.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.use((req, res, next) => {
 
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   
 
   res.header(
